2d442d98a0cd98ca9616f50f6da8d7fe20f92e8f
BambiTall <bambihuang313@gmail.com>
2023-07-22 17:11:52 +0800

    Add liff

---
+++ frontend/src/pages/LiffLogin.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { onMounted } from 'vue';
+import liff from "@line/liff";
+
+onMounted(async () => {
+  try {
+    await liff.init({ liffId: "2000144386-Ax8WZ8k2" });
+    console.log('LIFF OK');
+  } catch (err) {
+    console.log(`liff.state init error ${err}`);
+  }
+  // try {
+  //   await liff.init({ liffId: "2000144386-Ax8WZ8k2" });
+  //   if (!liff.isLoggedIn())
+  //     liff.login({ redirectUri: window.location.href });
+  // } catch (err) {
+  //   console.log(`liff.state init error ${err}`);
+  // }
+})
+</script>
+
+<template>
+  LIFF
+</template>
+
+<style scoped>
+</style>
