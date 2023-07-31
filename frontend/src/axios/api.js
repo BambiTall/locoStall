bac33d0b000b1d4f6ad7921ba9d86e9040381588
BambiTall <bambihuang313@gmail.com>
2023-07-31 22:17:41 +0800

    Update base url

--- frontend/src/axios/api.js
+++ frontend/src/axios/api.js
@@ -1,7 +1,15 @@
 import axios from 'axios';
 
+let baseUrl = '';
+
+if( import.meta.env.MODE === 'development' ){
+  baseUrl = 'localhost:5000'
+} else {
+  baseUrl = '107.167.190.202:5000'
+}
+
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api/',
+  baseURL: `http://${baseUrl}/api/`,
   headers: {
     "Content-Type": "application/json; charset=utf-8",
     Accept: "application/json",
