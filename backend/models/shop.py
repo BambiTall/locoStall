183b626988c33608428e389e190dfe3f56616b2d
Urik Rocksalt <610521224@gms.ndhu.edu.tw>
2023-08-01 15:04:53 +0000

    add cover in shop model

--- backend/models/shop.py
+++ backend/models/shop.py
@@ -1,5 +1,6 @@
 from ..extentions import db
 
+
 class Shop(db.Model):
     __tablename__ = 'shop'
 
@@ -7,6 +8,7 @@ class Shop(db.Model):
     slug = db.Column(db.String(100))
     rating = db.Column(db.Integer)
     shop_names = db.relationship('Shop_name', backref='shop', lazy=True)
+    cover = db.Column(db.String(255))
 
     def json(self):
         return {
@@ -14,4 +16,5 @@ class Shop(db.Model):
             'slug': self.slug,
             'rating': self.rating,
             'shop_names': [shop_name.json() for shop_name in self.shop_names],
+            'cover': self.cover,
         }
