7568715d4ac78dc7a8db95c5d5e9fc6a57d42634
BambiTall <bambihuang313@gmail.com>
2023-08-10 00:14:27 +0800

    Add descend to order list datas.

--- backend/routes/order.py
+++ backend/routes/order.py
@@ -25,7 +25,7 @@ def get_orders_list():
 @order_bp.route(f'{os.environ["API_BASE"]}/orders/user/<int:user_id>')
 def get_user_order_list(user_id):
     user = User.query.filter_by(id=user_id).first()
-    orders = Orders.query.filter_by(user_id=user_id)
+    orders = Orders.query.filter_by(user_id=user_id).order_by(Orders.updated_at.desc())
     order_list = [order.json() for order in orders]
 
     for order in order_list:
@@ -49,7 +49,7 @@ def get_user_order_list(user_id):
 @order_bp.route(f'{os.environ["API_BASE"]}/orders/manager/<int:user_id>')
 def get_manager_order_list(user_id):
     user = User.query.filter_by(id=user_id).first()
-    orders = Orders.query.filter_by(shop_id=user.shop_id)
+    orders = Orders.query.filter_by(shop_id=user.shop_id).order_by(Orders.created_at.desc())
     order_list = [order.json() for order in orders]
 
     for order in order_list:
