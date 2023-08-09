8803d337736031757a15d356d345ccde13acf6eb
Urik Rocksalt <610521224@gms.ndhu.edu.tw>
2023-08-09 04:09:02 +0000

    add user default lang, type

--- backend/models/user.py
+++ backend/models/user.py
@@ -1,15 +1,16 @@
 from ..extentions import db
 
+
 class User(db.Model):
     __tablename__ = 'user'
-    
+
     id = db.Column(db.Integer, primary_key=True)
     display_name = db.Column(db.String(100))
     mail = db.Column(db.String(100))
-    native_lang = db.Column(db.String(10))
+    native_lang = db.Column(db.String(10), default='en')
     line_id = db.Column(db.String(1000))
     password = db.Column(db.String(50))
-    type = db.Column(db.String(20))
+    type = db.Column(db.String(20), default='user')
     shop_id = db.Column(db.Integer)
 
     def json(self):
@@ -21,5 +22,5 @@ class User(db.Model):
             'line_id': self.line_id,
             'password': self.password,
             'type': self.type,
-            'shop_id': self.shop_id
+            'shop_id': self.shop_id,
         }
