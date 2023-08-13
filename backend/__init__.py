5fdc3470490f11f39bc5f36db2dcc2f1ad8e85db
BambiTall <bambihuang313@gmail.com>
2023-08-13 21:14:51 +0800

    open flask debug

--- backend/__init__.py
+++ backend/__init__.py
@@ -29,7 +29,7 @@ def create_app():
     app.config['SECRET_KEY']  = f'{os.environ.get("FLASK_SCREAT_KEY")}'
     app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(days=31)
     app.config['SESSION_COOKIE_SAMESITE'] = 'None'
-    app.config['DEBUG']
+    app.config['DEBUG'] = True
     session_cookie = SecureCookieSessionInterface().get_signing_serializer(app)
 
 
