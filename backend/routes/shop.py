241f54c1a0df3aeb2a3194773463cad347a11d4c
BambiTall <bambihuang313@gmail.com>
2023-08-15 23:09:01 +0800

    Add more info to shop list.

--- backend/routes/shop.py
+++ backend/routes/shop.py
@@ -13,9 +13,24 @@ shop_bp = Blueprint('shop_bp', __name__)
 # Get shop list
 @shop_bp.route(f'{os.environ["API_BASE"]}/<lang>/shops', methods=['GET'])
 def get_shop_list(lang):
-    shops = Shop_name.query.filter_by(lang=lang)
-    shop_list = [shop.json() for shop in shops]
-    return jsonify(shop_list)
+    shop_data = []
+    
+    shops = Shop_name.query.filter_by(lang=lang).all()
+    
+    for shop in shops:
+        shop_id = shop.shop_id
+        shop_info = Shop.query.get(shop_id)
+        
+        shop_data.append({
+            'shop_id': shop_id,
+            'slug': shop_info.slug,
+            'rating': shop_info.rating,
+            'name': shop.name,
+            'description': shop.description,
+            'cover': shop_info.cover
+        })
+    
+    return jsonify(shop_data)
 
 
 # Get shop detail
