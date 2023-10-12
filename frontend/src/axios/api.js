d63200316cb0f10aeed0df1010dc6fe490242b43
BambiTall <bambihuang313@gmail.com>
2023-10-13 06:48:18 +0800

    update api url

--- frontend/src/axios/api.js
+++ frontend/src/axios/api.js
@@ -2,10 +2,12 @@ import axios from 'axios';
 
 let baseUrl = '';
 
+baseUrl = 'http://34.152.28.2:5000'
+
 // if( import.meta.env.MODE === 'development' ){
 //   baseUrl = 'http://localhost:5000'
 // } else {
-  baseUrl = 'https://backend.locostall.shop'
+//   baseUrl = 'https://backend.locostall.shop'
 // }
 
 const api = axios.create({
